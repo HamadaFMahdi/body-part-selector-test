2023c622b69e382dbc763dabed6ffa0244f9b7f6
Hamada Fadil Mahdi <hamadamahdi2014@gmail.com>
2021-12-05 01:23:35 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="human-body">
+      <vue-body-part-selector 
+        :selection.sync="selection"
+        @click="handleClick"
+        :disabled="disabled"
+        :multiple="multiple"
+        :body-colour="bodyColour"
+        :selection-colour="selectionColour"
+        :showParts="{
+          'head': true,
+          'face': true,
+          'neck': true,
+          'shoulder-left': true,
+          'shoulder-right': true,
+          'arm-left': true,
+          'forearm-left': true,
+          'arm-right': true,
+          'forearm-right': true,
+          'chest-left': true,
+          'chest-right': true,
+          'belly-left': true,
+          'ribs-left': true,
+          'belly-right': true,
+          'belly': true,
+          'ribs-right': true,
+          'thigh-left': true,
+          'innerthigh-left': true,
+          'feet-left': true,
+          'calf-left': true,
+          'knee-left': true,
+          'thigh-right': true,
+          'genitalia': true,
+          'innerthigh-right': true,
+          'right-feet': true,
+          'calf-right': true,
+          'knee-right': true,
+          'elbow-right': true,
+          'hand-right': true,
+          'elbow-left': true,
+          'hands-left': true,
+          'armback-left': true,
+          'leg-left': true,
+          'buttock': true,
+          'loin': true,
+          'column': true,
+          'head-back': true,
+          'nape': true,
+          'armback-right': true,
+          'leg-right': true,
+          'back-right': true,
+          'clavicule-right': true,
+          'back-left': true,
+          'clavicule-left': true,
+        }"
+      />
+    </div>
+    <br>
+    Selection: {{selection}}
+    <br>
+    <br>
+    <strong>Options:</strong>
+    <br>
+    <input type="radio" v-model="disabled" :value="true">Disabled
+    <input type="radio" v-model="disabled" :value="false">Not Disabled
+    <br>
+    <input type="radio" v-model="multiple" :value="true">Multiple
+    <input type="radio" v-model="multiple" :value="false">Single
+    <br>
+    <label for="bodyColour"> Body Colour: </label>
+    <input id="bodyColour" type="text" v-model="bodyColour">
+    <br>
+    <label for="selectionColour"> Selection Colour: </label>
+    <input id="selectionColour" type="text" v-model="selectionColour">
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import VueBodyPartSelector from 'vue-body-part-selector';
 
-export default {
+export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    VueBodyPartSelector
+  },
+  data() {
+    return {
+      selection: '',
+      disabled: false,
+      multiple: false,
+      bodyColour: "#FF0000",
+      selectionColour: "#0000FF"
+    };
+  },
+  methods: {
+    handleClick(bodyPartClicked) {
+      console.log(bodyPartClicked)
+    }
   }
-}
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+  .human-body {
+    width: 300px;
+    height: 420px;
+  }
+
 </style>
